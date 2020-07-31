2d80911504c0fa19311836e24d86bb84baceb425
Ajaypal Singh <er.ajaypal@gmail.com>
2020-07-31 14:34:39 +1000

    tests: installed phpunit using composer, add gitignore to avoid adding vendor and add tests cases for Robot commands

---
+++ tests/RobotTest.php
@@ -0,0 +1,58 @@
+<?php
+use \PHPUnit\Framework\TestCase;
+use App\classes\Robot;
+
+class RobotTest extends TestCase
+{
+    public function testPlaceCommand()
+    {
+        $robot = new Robot();
+        $robot->set_position_direction(3,3,'north');
+
+        $this->assertSame(3, $robot->get_position_x());
+        $this->assertSame(3, $robot->get_position_y());
+        $this->assertSame('NORTH', $robot->get_direction());
+    }
+
+    public function testMoveCommand()
+    {
+        $robot = new Robot;
+        $robot->set_position_direction(3,3,'north');
+        $robot->move();
+
+        $this->assertSame(3, $robot->get_position_x());
+        $this->assertSame(4, $robot->get_position_y());
+        $this->assertSame('NORTH', $robot->get_direction());
+
+        $robot->move();
+        $this->assertSame(3, $robot->get_position_x());
+        $this->assertSame(4, $robot->get_position_y());
+        $this->assertSame('NORTH', $robot->get_direction());
+    }
+
+    public function testLeftCommand()
+    {
+        $robot = new Robot;
+        $robot->set_position_direction(0,0,'NORTH');
+        $robot->rotate_left();
+
+        $this->assertSame('WEST', $robot->get_direction());
+    }
+
+    public function testRightCommand()
+    {
+        $robot = new Robot;
+        $robot->set_position_direction(0,0,'NORTH');
+        $robot->rotate_right();
+
+        $this->assertSame('EAST', $robot->get_direction());
+    }
+
+    public function testReportCommand()
+    {
+        $robot = new Robot;
+        $robot->set_position_direction(0,0,'NORTH');
+        
+        $this->assertSame('Toy robot is at 0, 0, NORTH', $robot->get_current_position());
+    }
+}
